8a0202092f6cd5e25b96492f91ad23616495fd86
rolly14 <122947320+rolly14@users.noreply.github.com>
2024-11-18 20:41:26 +0000

    closures assignments

---
+++ closures_assignments/target/debug/closures_assignments.d
@@ -0,0 +1,1 @@
+/workspaces/rustfall2024/closures_assignments/target/debug/closures_assignments: /workspaces/rustfall2024/closures_assignments/src/main.rs
---
+++ closures_assignments/target/debug/deps/closures_assignments-114d2003ba446586.d
@@ -0,0 +1,5 @@
+/workspaces/rustfall2024/closures_assignments/target/debug/deps/closures_assignments-114d2003ba446586: src/main.rs
+
+/workspaces/rustfall2024/closures_assignments/target/debug/deps/closures_assignments-114d2003ba446586.d: src/main.rs
+
+src/main.rs:
